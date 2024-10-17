8008bcf02ad895170d1c3677227e2c6567c26f2b
Sonu-Delhi <sy9812672@gmail.com>
2024-10-17 21:16:26 +0530

    frontend env and common folder change

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const PORT = 8080
 
 export const connectDb = async()=>{
-    await mongoose.connect('mongodb+srv://root:root12345@cluster0.0oubsko.mongodb.net/').then(()=>{
+    await mongoose.connect(process.env.MONGODB_URI).then(()=>{
         console.log("Db connected");
         console.log(`Server is up and running on port http://loaclhost:${PORT}`);
         
